d2a1d53716337a2dd9127ee536a4d61c74f971f6
faisalabbas001 <faisalabbas7959@gmail.com>
2025-08-16 20:02:53 +0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
 			</div>
 			<br />
 			<br />
-			<ComputersCanvas />
+			{/* <ComputersCanvas /> */}
 
 			<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
 				<a href='#about'>
