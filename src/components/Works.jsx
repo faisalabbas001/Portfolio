fa55a907e16e59b26f0530dd08a57f51e8a60998
faisalabbas001 <faisalabbas7959@gmail.com>
2025-08-16 17:49:12 +0500

    chnage

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
@@ -10,11 +10,10 @@ const ProjectCard = ({ name, description, image, tags, source_code_link, index }
 	return (
 		<motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
 			<Tilt
-				options={{
-					max: 45,
-					scale: 1,
-					speed: 450,
-				}}
+				tiltMaxAngleX={45}
+				tiltMaxAngleY={45}
+				scale={1}
+				transitionSpeed={450}
 				className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
 				<div className="relative w-full h-[230px]">
 					<img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
